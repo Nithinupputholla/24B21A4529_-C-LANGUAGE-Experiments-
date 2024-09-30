8121b57e8126d6f74c897b758e6c850ac5a59719
Nithinupputholla <nithinupputholla@gmail.com>
2024-09-30 20:25:20 +0530

    Add files via upload

---
+++ Experiment3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+float principal, rate, time, interest;
+printf("Enter the principal amount: ");
+scanf("%f"
+, &principal);
+printf("Enter the rate: ");
+scanf("%f"
+, &rate);
+printf("Enter the time: ");
+scanf("%f"
+, &time);
+interest = (principal * rate * time) / 100;
+printf("The Simple Interest is: %.2f"
+, interest);
+return 0;
+}
